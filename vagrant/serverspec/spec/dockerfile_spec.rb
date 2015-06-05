97e44559e16e203a9bb59f1479a36bc291c1171a
Sharipov Ruslan <sharipov.ru@gmail.com>
2015-06-06 04:01:48 +0600

    remove specs

--- vagrant/serverspec/spec/dockerfile_spec.rb
+++ vagrant/serverspec/spec/dockerfile_spec.rb
@@ -11,28 +11,4 @@ describe "Dockerfile" do
     set :backend, :docker
     set :docker_image, @image.id
   end
-  
-  it "should have working directory" do
-    expect(@image.json["Config"]["WorkingDir"]).to eq("/data")
-  end
-
-  it "should expose mongodb ports" do
-    ports = [27017, 28017]
-
-    ports.each do |port|
-      expect(@image.json["Config"]["ExposedPorts"].has_key?("#{port}/tcp")).to eq(true)
-    end
-  end
-
-  describe package('mongodb-org') do
-    it { should be_installed }
-  end
-
-  describe service('mongod') do
-    it { should be_enabled }
-  end
-  
-  describe service('mongod') do
-    it { should be_running }
-  end
 end
